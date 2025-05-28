dd7a7168392cd16a86ac49dc3fe25c13f9345a58
Alex Medina <alexmedina@192.168.1.59>
2025-05-28 14:16:07 -0600

    modified src for img

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,7 +8,7 @@ export default function ProjectCard({ title, image, description, link } : Projec
     <div className="project-card flex flex-col justify-between p-4 border border-orange-500 rounded-lg shadow-md bg-gray-900 h-full min-h-[460px]">
       <div>
         <h3 className="text-lg font-semibold text-center py-2">{title}</h3>
-        <img src={image} alt={title} className="h-60 w-fit object-contain mx-auto" />
+        <img src={`${import.meta.env.BASE_URL}${image}`} alt={title} className="h-60 w-fit object-contain mx-auto" />
         <p className="text-center mt-2">{description}</p>
       </div>
       <a
