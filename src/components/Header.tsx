2cfa5d4dc097a4b6ec6d232ab9469e3294b4ff36
Alex Medina <alexmedina@192.168.1.88>
2025-05-31 09:19:35 -0600

    installed react-scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,18 +38,19 @@ export default function Header() {
               <li><a href="#about" onClick={toggleMenu} className="hover:text-orange-400 font-bold">About Me</a></li>
               <li><a href="#tech" onClick={toggleMenu} className="hover:text-orange-400 font-bold">Tech Stack</a></li>
               <li><a href="#projects" onClick={toggleMenu} className="hover:text-orange-400 font-bold">Projects</a></li>
-              <li><a href="#certifications" onClick={toggleMenu} className="hover:text-orange-400 font-bold">Certifications</a></li>
+              <li><a href="#contact" onClick={toggleMenu} className="hover:text-orange-400 font-bold">Contact</a></li>
             </ul>
           </nav>
         )}
       </header>
 
       {/* Hero */}
+      
       <section id="about" className="flex flex-col items-center justify-center bg-gray-900 text-white text-center rounded-lg shadow-2xl p-5 mt-10 w-11/12 md:w-2/3 mx-auto"> 
         <h1  className="text-lg text-center font-black">Hi, I'm Alejandro Medina</h1>
         <p className="text-4xl mt-5 text-orange-400">Creative Front-End Developer</p>
         <p className="mt-5 text-balance font-bold">
-          I’m from Jalisco, Mexico, with U.S. citizenship. I build responsive, accessible, and high-performance web apps using React, TypeScript, and TailwindCSS. I'm passionate about clean code, UX, and modern frontend practices. Open to remote roles and relocation.
+          I'm from Jalisco, Mexico, with U.S. citizenship. I build responsive, accessible, and high-performance web apps using React, TypeScript, and TailwindCSS. I'm passionate about clean code, UX, and modern frontend practices. Open to remote roles and relocation.
         </p>
       </section>
 
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -2,28 +2,31 @@ import { SiReact, SiTypescript, SiJavascript, SiHtml5, SiCss3, SiTailwindcss, Si
 
 export default function TechStack() {
   return (
-    <section id='tech' className="mt-20 mx-auto max-w-4xl px-4">
-      <h2 className="text-center font-bold text-3xl text-orange-400 mb-8">Tech Stack</h2>
-      
-      {/* Grid flexible que pasa a fila en pantallas md+ */}
-      <div className="grid grid-cols-3 sm:grid-cols-4 md:flex md:justify-center md:gap-10 gap-6">
-        {[ 
-          { Icon: SiReact, color: "text-cyan-400" },
-          { Icon: SiTypescript, color: "text-blue-500" },
-          { Icon: SiJavascript, color: "text-yellow-400" },
-          { Icon: SiHtml5, color: "text-orange-600" },
-          { Icon: SiCss3, color: "text-blue-700" },
-          { Icon: SiTailwindcss, color: "text-cyan-400" },
-          { Icon: SiVite, color: "text-yellow-500" },
-        ].map(({ Icon, color }, idx) => (
-          <div
-            key={idx}
-            className={`flex flex-col items-center justify-center ${color} hover:scale-110 transform transition-transform duration-300 cursor-pointer`}
-          >
-            <Icon className="text-7xl" />
+
+    <>
+        <section id='tech' className="mt-20 mx-auto max-w-4xl px-4">
+          <h2 className="text-center font-bold text-3xl text-orange-400 mb-8">Tech Stack</h2>
+          
+          {/* Grid flexible que pasa a fila en pantallas md+ */}
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:flex md:justify-center md:gap-10 gap-6">
+            {[ 
+              { Icon: SiReact, color: "text-cyan-400" },
+              { Icon: SiTypescript, color: "text-blue-500" },
+              { Icon: SiJavascript, color: "text-yellow-400" },
+              { Icon: SiHtml5, color: "text-orange-600" },
+              { Icon: SiCss3, color: "text-blue-700" },
+              { Icon: SiTailwindcss, color: "text-cyan-400" },
+              { Icon: SiVite, color: "text-yellow-500" },
+            ].map(({ Icon, color }, idx) => (
+              <div
+                key={idx}
+                className={`flex flex-col items-center justify-center ${color} hover:scale-110 transform transition-transform duration-300 cursor-pointer`}
+              >
+                <Icon className="text-7xl" />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </section>
+        </section>
+    </>
   );
 }
